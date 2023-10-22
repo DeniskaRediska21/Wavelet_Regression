1e4cc2a27aaa4dd1236391ae6fc257dd66605b42
DeniskaRediska21 <denisgontar99@gmail.com>
2023-10-22 18:38:17 +0300

    Addea torch implementations of wavelet transform, Started implementing custom network

---
+++ custom_net.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from wavelet_transform_module import Torch_Morlet_Transform, Torch_Morlet_Transform_imag
+import numpy as np
+class Net(nn.Module):
+    def __init__(self,freq = 1, discretness= 0.01, number_of_scycles = 2):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv1d(1, 32, 3)
+        self.morlet_r = Torch_Morlet_Transform(freq = freq, discretness = discretness, number_of_scycles = number_of_scycles) 
+        self.morlet_i = Torch_Morlet_Transform_imag(freq = freq, discretness = discretness, number_of_scycles = number_of_scycles) 
+        self.linear1 = nn.Linear(2048,512).to(torch.cfloat)
+
+        self.linear2 = nn.Linear(512,128)
+
+        self.linear3 = nn.Linear(128,1)
+    # x represents our data
+    def forward(self, x):
+        WT_i1 = self.morlet_i(x)
+        fft_i1 = torch.fft.fft(WT_i1)
+        WT_i2 = self.morlet_i(WT_i1)
+        fft_i2 = torch.fft.fft(WT_i2)
+        
+        WT_r1 = self.morlet_r(x)
+        fft_r1 = torch.fft.fft(WT_r1)
+        WT_r2 = self.morlet_r(WT_r1)
+        fft_r2 = torch.fft.fft(WT_r2)
+        y = self.linear1(torch.cat((WT_i1,fft_i1,WT_i2,fft_i2,WT_r1,fft_r1,WT_r2,fft_r2)))
+        
+        return y.shape
+
+
+
+signal = np.sin(np.arange(0,100,0.001))
+signal = torch.Tensor(signal).to('cuda:0')
+model = Net(freq = 10, discretness= 0.01).to('cuda:0')
+
+print(model(signal))
+
+
+
--- wavelet_transform_module.py
+++ wavelet_transform_module.py
@@ -1,15 +1,50 @@
 import cupy as cp
+from torchaudio.transforms import FFTConvolve as torch_fftconvolve
 from cupyx.scipy.signal import fftconvolve
 from numpy import exp
 import numpy as np
 import torch
 
-class Morlet_Transform(torch.nn.Module):
-    def __init__(self,freq= 1, discreteness= 0.01, number_of_scycles =2):
+
+class Torch_Morlet_Transform(torch.nn.Module):
+    def __init__(self,freq= 1, discretness= 0.01, number_of_scycles =2):
         super().__init__()
         morlet_wavelet = lambda freq,time,number_of_scycles : exp(1j*freq*time)*exp((-time**2*freq**2)/(2*number_of_scycles**2))
         boundaries = (number_of_scycles+1)*3 / freq
-        time = np.arange(-boundaries,boundaries,discreteness)
+        time = np.arange(-boundaries,boundaries,discretness)
+        self.wavelet = torch.Tensor(np.real(np.array(np.squeeze([morlet_wavelet(freq,time,number_of_scycles)])))).cuda()
+
+
+    def forward(self, signal):
+        convolve =torch_fftconvolve('valid') 
+        convolution =convolve(signal,self.wavelet)
+
+        return convolution
+
+
+
+class Torch_Morlet_Transform_imag(torch.nn.Module):
+    def __init__(self,freq= 1, discretness= 0.01, number_of_scycles =2):
+        super().__init__()
+        morlet_wavelet = lambda freq,time,number_of_scycles : exp(1j*freq*time)*exp((-time**2*freq**2)/(2*number_of_scycles**2))
+        boundaries = (number_of_scycles+1)*3 / freq
+        time = np.arange(-boundaries,boundaries,discretness)
+        self.wavelet = torch.Tensor(np.imag(np.array(np.squeeze([morlet_wavelet(freq,time,number_of_scycles)])))).cuda() 
+
+
+    def forward(self, signal):
+        convolve =torch_fftconvolve('valid') 
+        convolution =convolve(signal,self.wavelet)
+
+        return convolution
+
+
+class Morlet_Transform(torch.nn.Module):
+    def __init__(self,freq= 1, discretness= 0.01, number_of_scycles =2):
+        super().__init__()
+        morlet_wavelet = lambda freq,time,number_of_scycles : exp(1j*freq*time)*exp((-time**2*freq**2)/(2*number_of_scycles**2))
+        boundaries = (number_of_scycles+1)*3 / freq
+        time = np.arange(-boundaries,boundaries,discretness)
         self.wavelet = cp.real(cp.array(np.squeeze([morlet_wavelet(freq,time,number_of_scycles)]))) 
 
 
@@ -21,9 +56,25 @@ class Morlet_Transform(torch.nn.Module):
         return convolution
 
 
+class Morlet_Transform_imag(torch.nn.Module):
+    def __init__(self,freq= 1, discretness= 0.01, number_of_scycles =2):
+        super().__init__()
+        morlet_wavelet = lambda freq,time,number_of_scycles : exp(1j*freq*time)*exp((-time**2*freq**2)/(2*number_of_scycles**2))
+        boundaries = (number_of_scycles+1)*3 / freq
+        time = np.arange(-boundaries,boundaries,discretness)
+        self.wavelet = cp.imag(cp.array(np.squeeze([morlet_wavelet(freq,time,number_of_scycles)]))) 
+
+
+    def forward(self, signal):
+        convolution = fftconvolve(cp.array(signal),self.wavelet,'valid')
+
+        convolution = convolution.get()
+
+        return convolution
+
         
 #import matplotlib.pyplot as plt
-#model = Morlet_Transform(freq = 10, discreteness= 0.01)
+#model = Morlet_Transform(freq = 10, discretness= 0.01)
 #
 #signal = np.sin(np.arange(0,100,0.001))
 #convolution = model(signal)
